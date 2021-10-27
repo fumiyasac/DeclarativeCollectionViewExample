b48b17a936ba9cca0f5d7afd1a1823dfe50223a7
Fumiya Sakai <just1factory@gmail.com>
2021-10-27 14:14:21 +0900

    セッティング画面を追加

---
+++ DeclarativeCollectionViewExample/ViewController/SettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingViewController.swift
+//  DeclarativeCollectionViewExample
+//
+//  Created by 酒井文也 on 2021/10/27.
+//
+
+import UIKit
+
+final class SettingViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
